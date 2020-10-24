ee253c5e5600ae21d2a160eb2a25f1bb21b8d3e4
joeappleton18 <joe.appleton@solent.ac.uk>
2020-10-24 16:59:11 +0100

    updated seeder

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+const chalk = require("chalk");
+
+/**
+ * Controllers (route handlers).
+ */
+const tasterController = require("./controllers/taster");
+const tastingController = require("./controllers/tasting");
+
 const app = express();
 app.set("view engine", "ejs");
 
@@ -8,18 +17,41 @@ app.set("view engine", "ejs");
  * notice above we are using dotenv. We can now pull the values from our environment
  */
 
-const { WEB_PORT } = process.env;
+const { WEB_PORT, MONGODB_URI } = process.env;
+
+/**
+ * connect to database
+ */
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+  console.log(
+    "MongoDB connection error. Please make sure MongoDB is running.",
+    chalk.red("✗")
+  );
+  process.exit();
+});
 
 app.use(express.static(path.join(__dirname, "public")));
-
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.get("/tasters", (req, res) => {
-  res.render("tasters");
+app.get("/create-taster", (req, res) => {
+  res.render("create-taster");
 });
 
+app.get("/tasters", tasterController.list);
+app.get("/tasters/delete/:id", tasterController.delete);
+
+app.get("/tastings", tastingController.list);
+app.get("/tastings/delete/:id", tastingController.delete);
+
+
 app.listen(WEB_PORT, () => {
-  console.log(`Example app listening at http://localhost:${WEB_PORT}`);
+  console.log(
+    `Example app listening at http://localhost:${WEB_PORT}`,
+    chalk.green("✓")
+  );
 });
---
+++ controllers/taster.js
@@ -0,0 +1,22 @@
+const Taster = require("../models/Taster");
+
+exports.list = async (req, res) => {
+  try {
+    const tasters = await Taster.find({});
+    res.render("tasters", { tasters: tasters });
+  } catch (e) {
+    res.status(404).send({ message: "could not list tasters" });
+  }
+};
+
+exports.delete = async (req, res) => {
+  const id = req.params.id;
+  try {
+    await Taster.findByIdAndRemove(id);
+    res.redirect("/tasters");
+  } catch (e) {
+    res.status(404).send({
+      message: `could not delete  record ${id}.`,
+    });
+  }
+};
