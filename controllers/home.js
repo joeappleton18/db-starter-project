539c65b21b2571040a8a50b3c751a8c1cb173c90
joeappleton18 <joe.appleton@solent.ac.uk>
2020-11-27 10:09:52 +0000

    completed the seeder update

--- controllers/home.js
+++ controllers/home.js
@@ -4,15 +4,7 @@ exports.list = async (req, res) => {
     console.log(req.session);
     try {
 
-        /**
-         *
-            db.tastings.aggregate([
-                      {$match:  {taster_name: { $ne: null }}},
-                      {$group: {_id: "$taster_name", total: { $sum: 1} }},
-                      {$count: "number of tasters"}
-                      ])
-         */
-
+    
         const totalTastings = await Tasting.find({}).count();
         const totalCountries = await Tasting.aggregate([
             { $group: { _id: "$country", total: { $sum: 1 } } },
@@ -33,6 +25,7 @@ exports.list = async (req, res) => {
         res.render("index", { tasterCountSummary: tasterCountSummary, totalTastings: totalTastings, totalTasters: tasterCountSummary.length, totalCountries: totalCountries[0].total });
 
     } catch (e) {
+        console.log(e);
         res.status(404).send({
             message: `error rendering page`,
         });
